58fb6f0e95b6fb0e38982932ec1555c3079e5699
Elliot Hatch <elliot.hatch@gmail.com>
2017-07-22 17:31:24 -0600

    First iteration of clock, add test mode

--- clock-display.go
+++ clock-display.go
@@ -1,6 +1,8 @@
 package main;
 import (
 	"time"
+	//"math"
+	//"fmt"
 	"github.com/fresh4less/neopixel-display/neopixeldisplay"
 )
 
@@ -14,7 +16,7 @@ func NewClockDisplay(pixelDisplay neopixeldisplay.PixelDisplay) *ClockDisplay {
 	return &ClockDisplay{
 		pixelDisplay,
 		neopixeldisplay.NewScreenView(pixelDisplay, 0, minutesLedCount, 1, 1.0, neopixeldisplay.Error),
-		neopixeldisplay.NewScreenView(pixelDisplay, 0, hoursLedCount, 1, 1.0, neopixeldisplay.Error),
+		neopixeldisplay.NewScreenView(pixelDisplay, minutesLedCount, hoursLedCount, 1, 1.0, neopixeldisplay.Error),
 	}
 }
 
@@ -25,9 +27,21 @@ func (cd *ClockDisplay) displayTime(time time.Time) {
 	minutesFrame.SetAll(neopixeldisplay.MakeColor(0,0,0))
 	hoursFrame.SetAll(neopixeldisplay.MakeColor(0,0,0))
 
-	minutesFrame.Set(time.Minute(), 0, neopixeldisplay.MakeColor(255,255,255), neopixeldisplay.Error)
-	hoursFrame.Set(time.Hour()%12, 0, neopixeldisplay.MakeColor(255,255,255), neopixeldisplay.Error)
-	hoursFrame.Set((time.Hour()%12)+1, 0, neopixeldisplay.MakeColor(255,255,255), neopixeldisplay.Error)
+	for i := 0; i < time.Minute(); i++ {
+		minutesFrame.Set(58-i, 0, neopixeldisplay.MakeColor(255, 0, 100), neopixeldisplay.Error)
+	}
+	hoursColor := neopixeldisplay.MakeColor(118, 0, 255)
+	//if time.Hour() != 0 {
+	hourIndex := (hoursLedCount) - int(
+		(hoursLedCount*((float64(time.Hour()%12)/12.0) +
+						(float64(time.Minute()%60)/60.0)/12.0)))
+	for i := 0; i < hourIndex; i++ {
+		hoursFrame.Set(i, 0, hoursColor, neopixeldisplay.Error)
+	}
+	//hoursFrame.Set(hourIndex, 0, hoursColor, neopixeldisplay.Error)
+	//hoursFrame.Set((hourIndex-1)%hoursLedCount, 0, hoursColor, neopixeldisplay.Error)
+	//hoursFrame.Set((hourIndex-2)%hoursLedCount, 0, hoursColor, neopixeldisplay.Error)
+	//}
 
 	cd.minutesScreen.Draw()
 	cd.hoursScreen.Draw()
--- tock-clock.go
+++ tock-clock.go
@@ -3,14 +3,17 @@ package main
 import (
 	"flag"
 	"github.com/fresh4less/neopixel-display/neopixeldisplay"
+	"time"
 )
 
-const minutesLedCount = 60;
-const hoursLedCount = 24;
+const minutesLedCount = 59;
+const hoursLedCount = 42;
 
 func main() {
 	var display neopixeldisplay.PixelDisplay
 	noled := flag.Bool("noled", false, "If set, displays the clock as 256-color terminal stdout")
+	test := flag.Bool("test", false, "Displays time at a rate determined by test-delay flag")
+	testDelay := flag.Float64("test-delay", 1.0/60.0, "Delay in seconds between 'minutes' when running test command")
 	flag.Parse()
 	if *noled {
 		display = neopixeldisplay.NewConsoleColorDisplay(minutesLedCount+hoursLedCount, [][]int{[]int{minutesLedCount,1}, []int{hoursLedCount,1}})
@@ -18,6 +21,19 @@ func main() {
 		display = neopixeldisplay.NewNeopixelDisplay(18, minutesLedCount+hoursLedCount, 255)
 	}
 	clockDisplay := NewClockDisplay(display)
+
+	if *test {
+		for true {
+			for h := 0; h < 24; h++ {
+				for m := 0; m < 60; m++ {
+					clockDisplay.displayTime(time.Date(0,0,0,h,m,0,0, time.Local))
+					time.Sleep(time.Duration(*testDelay*float64(time.Second)))
+				}
+			}
+		}
+	}
+
+
 	clock := NewClockTimer(nil)
 	for(true) {
 		now := <-clock.C
